a6788d7f41b797b8c52e8cf0cd6d0a2063006106
Leobardo <leo.amarillas@gmail.com>
2023-11-28 21:06:12 -0700

    form register validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   // styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+
+  registroForm: FormGroup;
+
+
+  /**
+   *
+   */
+  constructor(private fb: FormBuilder) {
+    //super();
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });   
+  }
+
+  crearUsuario() {
+    console.log(this.registroForm);
+    console.log(this.registroForm.invalid);
+    console.log(this.registroForm.value);
+  }
 
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAVQ46Hp2f5uqk5kc2XWUYJW-A--Hw2D1k",
+    authDomain: "ingreso-egreso-app-15d4c.firebaseapp.com",
+    projectId: "ingreso-egreso-app-15d4c",
+    storageBucket: "ingreso-egreso-app-15d4c.appspot.com",
+    messagingSenderId: "493295139533",
+    appId: "1:493295139533:web:968a8c69ab62bdb4520784",
+    measurementId: "G-F8Y7Z78PLF"
+  }
+};
